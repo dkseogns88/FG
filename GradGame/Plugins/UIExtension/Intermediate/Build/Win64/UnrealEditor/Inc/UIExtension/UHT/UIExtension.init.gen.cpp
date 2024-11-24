aa0cf12ebb4179cb03f09e679698afd646767f2b
An Dae Hun <dkseogns88@naver.com>
2024-11-24 23:10:27 +0900

    Statue light, logic

--- GradGame/Plugins/UIExtension/Intermediate/Build/Win64/UnrealEditor/Inc/UIExtension/UHT/UIExtension.init.gen.cpp
+++ GradGame/Plugins/UIExtension/Intermediate/Build/Win64/UnrealEditor/Inc/UIExtension/UHT/UIExtension.init.gen.cpp
@@ -17,7 +17,7 @@ void EmptyLinkFunctionForGeneratedCodeUIExtension_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x5DC4C55C,
+				0x376B2177,
 				0xE52C8F25,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -25,5 +25,5 @@ void EmptyLinkFunctionForGeneratedCodeUIExtension_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_UIExtension.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_UIExtension(Z_Construct_UPackage__Script_UIExtension, TEXT("/Script/UIExtension"), Z_Registration_Info_UPackage__Script_UIExtension, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x5DC4C55C, 0xE52C8F25));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_UIExtension(Z_Construct_UPackage__Script_UIExtension, TEXT("/Script/UIExtension"), Z_Registration_Info_UPackage__Script_UIExtension, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x376B2177, 0xE52C8F25));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
